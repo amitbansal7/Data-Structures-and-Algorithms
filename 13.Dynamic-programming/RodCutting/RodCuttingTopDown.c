e5d24f390e4a1e171f67a3336de0083d1ed15ac8
Amit Bansal <007amitbansal@gmail.com>
2017-04-18 20:16:05 +0530

    Rod cutting

--- 13.Dynamic-programming/RodCutting/RodCuttingTopDown.c
+++ 13.Dynamic-programming/RodCutting/RodCuttingTopDown.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#define NIL -1
 
 //Top-down approach with memoization.
-// Not working.
 
 int max(int a,int b){ return a>b?a:b;}
 
 int utilMemoizedCutRod(int p[], int n, int memo[])
 {
-	if(memo[n] >= 0)
+	if(memo[n] != NIL)
 		return memo[n];
 	int q;
 	
@@ -18,22 +18,23 @@ int utilMemoizedCutRod(int p[], int n, int memo[])
 
 	else
 	{
-		q = INT_MIN;
+		q = NIL;
 
 		for(int i=0;i<n;i++)
-			q = max(q, p[i]+utilMemoizedCutRod(p,n-i-1,memo));
+			q = max(q, p[i] + utilMemoizedCutRod(p,n-i-1,memo));
 	}
-	memo[n] = q;
 
+	memo[n] = q;
+	
 	return q;
 }
 
 int MemoizedCutRod(int p[], int n)
 {
-	int memo[n];
+	int memo[n+1];
 
-	for(int i=0;i<n;i++)
-		memo[i] = INT_MIN;
+	for(int i=0;i<=n;i++)
+		memo[i] = NIL;
 
 	return utilMemoizedCutRod(p, n, memo);
 }
@@ -42,7 +43,7 @@ int main()
 {
 	int p[10] = {1,5,8,9,10,17,17,20,24,30};
 
-	printf("%d\n",MemoizedCutRod(p,5));
+	printf("%d\n",MemoizedCutRod(p,7));
 
 	return 0;
 }
