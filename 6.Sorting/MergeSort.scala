686daf01fe1ae1a63c3dbb30f5ff15902708e176
Amit Bansal <007amitbansal@gmail.com>
2018-02-11 16:47:54 +0530

    Merge Sort

--- 6.Sorting/MergeSort.scala
+++ 6.Sorting/MergeSort.scala
@@ -1,16 +1,16 @@
-def mergeSort(xs :List[Int]): List[Int] = {
+def mergeSort[T](xs :List[T])(lt: (T, T) => Boolean): List[T] = {
     val n = xs.length
     if(n <= 1) xs
     else{
         val (first, second) = xs.splitAt(n/2)
-        def merge(xs:List[Int], ys:List[Int]): List[Int] = (xs, ys) match {
+        def merge(xs:List[T], ys:List[T]): List[T] = (xs, ys) match {
             case (Nil, ys) => ys
             case (xs, Nil) => xs
             case (x::xs, y::ys) => 
-                if (x <= y)  x :: merge(xs,y :: ys)
+                if (lt(x, y))  x :: merge(xs,y :: ys)
                 else y :: merge(x::xs, ys)
         }
-        merge(mergeSort(first),mergeSort(second))
+        merge(mergeSort(first)(lt),mergeSort(second)(lt))
     }
 }
-println(mergeSort(6::5::4::3::2::1::Nil))
+println(mergeSort(6::5::4::3::2::1::Nil)((a:Int, b:Int)=> a<=b))
